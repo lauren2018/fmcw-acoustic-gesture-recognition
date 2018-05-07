1007cfba6f47bb13cf76afe6bbece0528d62ccce
XIE <xiewt86@gmail.com>
2018-05-07 19:56:48 +0800

    important commit

--- fmcw.m
+++ fmcw.m
@@ -1,13 +1,13 @@
 % Author: Wentao Xie, Meng Zhou and Xiaotong Zhang
 clear;
-close all;
+% close all;
 
 %% --------------- Constants & configuration ------------------
 
 Fs = 48000;
-initFreq = 17e3;
-finlFreq = 19.5e3;	
-timeInterv = 0.04;
+initFreq = 16e3;
+finlFreq = 22e3;	
+timeInterv = 0.02;
 
 % -------------------------------------------------------------
 %% --------------- A frame of chirp signal --------------------
@@ -22,31 +22,71 @@ N = length(chirpSlice) ;
 H = hanning(N);
 hanChirp = chirpSlice.*H';
 finChirp=[];
-for i=1:250
+for i=1:1000
     finChirp=[finChirp,hanChirp];
 end
 
 % -------------------------------------------------------------
 %% ---------------------- Write / read ------------------------
 
-douTrackSig = [finChirp', zeros(1, length(finChirp))'];
-% audiowrite('test.wav', douTrackSig, Fs);
-rcvChirp = pcmread('move2.pcm');
-
+douTrackSig = [ zeros(1, length(finChirp))', finChirp' ];
+audiowrite('test2.wav', douTrackSig, Fs);
+rcvChirp = pcmread('t.pcm');
+[b, a] = butter(10, 15500/(Fs/2), 'high');
+rcvChirp = filtfilt(b, a, rcvChirp);
 % -------------------------------------------------------------
 %% ----------------------- Dechirp ----------------------------
-
+locs = [];
+sig = {};
+S = [];
 [f,idx] = xcorr(rcvChirp, finChirp);
 [~,I] = max(f);
 offset = idx(I);
-rcvChirp = rcvChirp(offset+1 : offset+length(finChirp));
+rcvChirp = rcvChirp(offset+1+1 : offset+length(finChirp)+1);
 deChirp = finChirp .* rcvChirp';
+% figure;
 for i = 1:length(hanChirp):length(finChirp)
-      f = abs(fft(deChirp(i:i+length(hanChirp)-1)));
-      plot(f(1:100))
-      ylim([0, 290000])
-      xlim([0, 100])
-      drawnow
+      f = abs(fft(deChirp(i:i+length(hanChirp)-1), 8192 *2));
+%       plot(f(1:1000))
+%       ylim([0, 250000])
+%       xlim([0, 1000])
+%       drawnow
+%       pause(0.1)
+      sig = [sig, f(1:1000)];
+      S = [S, f(1:100)'];
+      [pks, lcs] = findpeaks(f(1:1000));
+%       locs = [locs, lcs(1)];
 end
 
 % -------------------------------------------------------------
+idx = [];
+D = [];
+for i = 1 : length(sig)-1
+    t = sig{i+1}-sig{i};
+    t = abs(t - mean(t));
+    D = [D, t(1:20)'];
+    win = 5;
+% %     tt = [];
+% %     for k = 1 : length(t)-win
+% %         tt = [tt, sum(t(k:k+win).^1)];
+% %     end
+%     plot(t)
+% %      ylim([0, 0.1])
+%      xlim([0, 1000])
+%     drawnow 
+%     pause(0.2)
+%     [~, j] = sort(t);
+    [~, j] = max(t);
+%     [pks, locs] = findpeaks(t);
+%     [~, l] = max(pks);
+    idx = [idx, j];
+   
+end
+figure; plot(idx)
+win = 50;
+ii = [];
+for i = 1:length(idx)-win
+    ii = [ii, sum(idx(i:i+win-1))];
+end
+figure
+plot(ii)
